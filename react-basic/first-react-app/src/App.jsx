11fd675f8cf4b25de2e906915b5849f68f33e665
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2024-11-14 15:06:18 -0700

    renamed

---
+++ react-basic/first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header.jsx'
+import Footer  from './Footer.jsx'
+import IItem from './IItem.jsx';
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <IItem></IItem>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App
---
+++ react-basic/first-react-app/src/Header.jsx
@@ -0,0 +1,6 @@
+function Header(){
+return(
+    <header>my first app</header>
+);
+}
+export default Header
