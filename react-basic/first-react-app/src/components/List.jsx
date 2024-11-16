e7dc8c0e6529408a858e338a2dee5b887306b1f3
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2024-11-15 23:22:51 -0700

    dfd

--- react-basic/first-react-app/src/components/List.jsx
+++ react-basic/first-react-app/src/components/List.jsx
@@ -1,12 +1,24 @@
 function List(){
-    const allowedDevices = ["Mobile","Laptop","Watch","Pen Drive"];
-    allowedDevices.sort()// this will sort ascending order
-    allowedDevices.sort().reverse();// will sort in descending order
-    const listItem = allowedDevices.map(device => <li key = {device}>{device}</li>);
+
+    //array of employee object
+    const employees = [
+        {id:1, name:"Rakesh"},
+        {id:4, name:"Mayank"},
+        {id:2,name:"Sunil"},
+        {id:3, name:"Ram"}
+    ];
+  employees.sort((emp1,emp2) => emp1.id - emp2.id) // this will sort ascending order
+  employees.sort((emp1,emp2) => emp2.id - emp1.id)// will sort in descending order
+
+  // sorting with emp name
+  employees.sort((emp1,emp2) => emp1.name.localeCompare(emp2.name)); // asc
+  employees.sort((emp1,emp2) => emp2.name.localeCompare(emp1.name)); // desc
+   // apply filter
+   const filteredEmp = employees.filter(emp => emp.id>2); 
+    const empList = filteredEmp.map(emp => <li key = {emp.id}>{emp.name} &nbsp <b>{emp.id}</b></li>);
    
-    // return (<ol>{listItem}</ol>);
-   //Un Ordered list
-   return (<ul>{listItem}</ul>)
+    
+   return (<ul>{empList}</ul>)
 
 }
 export default List
