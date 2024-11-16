f3dbfac72e97beea32ce33f92c16aa04b894094d
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2024-11-16 16:10:15 -0700

    sorting and filtering 2

--- react-basic/first-react-app/src/App.jsx
+++ react-basic/first-react-app/src/App.jsx
@@ -1,9 +1,23 @@
 import List from './components/List.jsx'
 function App() {
-  // except string all value like boolean integer ...etc we will pass in {} braces
+  //array of employee object
+  const ftEmployees = [
+    {id:1, name:"Rakesh"},
+    {id:4, name:"Mayank"},
+    {id:2,name:"Sunil"},
+    {id:3, name:"Ram"}
+];
+
+const contractor = [
+  {id:51, name:"Ramesh"},
+  {id:52, name:"Mahesh"},
+  {id:53,name:"Suresh"},
+  {id:54, name:"Ganesh"}
+];
   return (
     <>
-   <List></List>
+   <List employees={ftEmployees} category="FT"></List>
+   <List employees={contractor} category="CONTRACTOR"></List>
   </>
   );
 }
--- react-basic/first-react-app/src/components/List.jsx
+++ react-basic/first-react-app/src/components/List.jsx
@@ -1,24 +1,16 @@
-function List(){
 
-    //array of employee object
-    const employees = [
-        {id:1, name:"Rakesh"},
-        {id:4, name:"Mayank"},
-        {id:2,name:"Sunil"},
-        {id:3, name:"Ram"}
-    ];
-  employees.sort((emp1,emp2) => emp1.id - emp2.id) // this will sort ascending order
-  employees.sort((emp1,emp2) => emp2.id - emp1.id)// will sort in descending order
+function List(props){
 
-  // sorting with emp name
-  employees.sort((emp1,emp2) => emp1.name.localeCompare(emp2.name)); // asc
-  employees.sort((emp1,emp2) => emp2.name.localeCompare(emp1.name)); // desc
-   // apply filter
-   const filteredEmp = employees.filter(emp => emp.id>2); 
-    const empList = filteredEmp.map(emp => <li key = {emp.id}>{emp.name} &nbsp <b>{emp.id}</b></li>);
-   
+    const category = props.category;
+    const empList = props.employees;
+    const empListItem = empList.map(emp => <li key={emp.id}>{emp.name}</li>)
+    return (
+    <>
+    <h3 className = "emp-category">{category}</h3>
+    <ul className="emp-items">{empListItem}</ul>
+    </>
     
-   return (<ul>{empList}</ul>)
+)
 
 }
 export default List
