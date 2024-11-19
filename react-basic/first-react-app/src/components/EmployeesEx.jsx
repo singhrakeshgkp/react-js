381ec0fcb23197ed93d454105c8a2121c8b62cdf
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2024-11-18 23:14:19 -0700

    dfdf

--- react-basic/first-react-app/src/components/EmployeesEx.jsx
+++ react-basic/first-react-app/src/components/EmployeesEx.jsx
@@ -1,28 +1,43 @@
 import React, {useState} from "react";
 function EmployeesEx(){
 
-    const [employees, setEmployees] = useState(["Rakesh Singh","Amit Kumar","Suryadev Singh"]);
+    const [employees, setEmployees] = useState([]);
+    const [name,setName] = useState("");
+    const [salary, setSalary] = useState(0);
+    const [address,setAddress] = useState("");
+    
     function addEmployee(){
-        const empName = document.getElementById("empInput").value;
-        document.getElementById("empInput").value="";
-        setEmployees(e=>[...e,empName]);//... spread operator to copy existing value.
-
-
+        const newEmp = {name: name,salary: salary,address: address};
+        setEmployees(e=>[...e,newEmp]);
+        //after adding/submitting form reset value
+        setName("");
+        setSalary(0);
+        setAddress("");
     }
-
     function deleteEmployee(index){
-       // setEmployees(employees.filter((e,i) => i !=index));
-       // e can be replaced with '_' cahr if not being used. this is best practice
         setEmployees(employees.filter((_,i) => i !=index));
     }
+    
+    function salaryChangeHandler(event){
+        setSalary(event.target.value);
+    }
+    function nameChanngeHanler(event){
+        setName(event.target.value);
+    }
+    function addressChangeHandler(event){
+        setAddress(event.target.value);
+    }
+    
     return (
         <div>
         <h3>Employees</h3>
         <ul>
-            {employees.map((empName,index)=> <li key={index} onClick={()=>deleteEmployee(index)}>{empName}</li>
+            {employees.map((emp,index)=> <li key={index} onClick={()=>deleteEmployee(index)}>{emp.name} {emp.salary} {emp.address}</li>
             )}
         </ul>
-        <input type="text" id = "empInput" placeholder="Please Enter Employee Name"></input>
+        <input type="text" value={name} onChange={nameChanngeHanler} placeholder="Please Enter Name"></input>
+        <input type="number" value={salary} onChange={salaryChangeHandler} placeholder="Please Enter Salary"></input>
+        <input type="text" value ={address} onChange={addressChangeHandler} placeholder="Please Enter Address"></input>
         <button onClick={addEmployee}>Add Employee</button>
         </div>
     );
