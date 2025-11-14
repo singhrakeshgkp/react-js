ed48ba729b9570da0b11b4980a346dabe21abb2a
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2025-11-14 10:46:39 -0700

    form handling

--- ex-component/src/App.jsx
+++ ex-component/src/App.jsx
@@ -2,17 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import InlineCssEx from './components/InlineCssEx'
-import './appStyle.css'
-import styles from './appStyle.module.css'
+import StudentForm from './components/StudentForm'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1 className='error'>error msg</h1>
-      <h1 className={styles.success}> Success message </h1>
-     <InlineCssEx></InlineCssEx>
+     <StudentForm></StudentForm>
+   
     </div>
   )
 }
