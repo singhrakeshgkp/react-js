738a8a19c4b4a4a85a4c63600129e342133c8d78
Rakesh singh <rakeshsingh@mac.mynetworksettings.com>
2025-11-20 19:00:33 -0700

    reducer data ffkdfjf

--- ex-component/src/App.jsx
+++ ex-component/src/App.jsx
@@ -1,38 +1,16 @@
-import React, { useState,useReducer } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Comp1 from './components/Comp1'
-import Comp2 from './components/Comp2'
-import Comp3 from './components/Comp3'
- 
-export const CountContext = React.createContext()
-const initialState = 0
-const reducer = (state,action) => {
-    switch(action){
-        case 'increment' :
-            return state+1
-        case 'decrement' :
-            return state-1
-        case 'reset' :
-            return initialState
-        default :
-            return state
-        
-    }
-}
-
+import UseStateDataFetchComp from './components/UseStateDataFetchComp'
+import UseReducerDataFetchComp from './components/UseReducerDataFetchComp'
 function App() {
-  const [count, dispatch] = useReducer(reducer,initialState)
   return (
-    <CountContext.Provider value={{countState: count, countDispatch: dispatch}}>
+
     <div>
-      Count - {count}
-    <Comp1></Comp1>
-    <Comp2></Comp2>
-    <Comp3></Comp3>
+      <UseStateDataFetchComp></UseStateDataFetchComp>
+      <UseReducerDataFetchComp></UseReducerDataFetchComp>
     </div>
-  </CountContext.Provider>
   )
 }
 
